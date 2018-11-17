7f55cfe7a717bbf6307eba4d0251a4ec5fc722db
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-17 04:56:36 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,12 @@
 class Project
   attr_accessor :backers, :title
-  
+
   def initialize(title)
     @backers = []
     @title = title
+  end
+
+  def add_backer(backer)
+    @backers << backer
   end
 end
