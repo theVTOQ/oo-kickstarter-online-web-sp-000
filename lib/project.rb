eaf1d3a9b97600b69ca93464a135d80efb4b5aa0
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-17 05:06:55 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,6 +8,6 @@ class Project
 
   def add_backer(backer)
     @backers << backer if !@backers.include?(backer)
-    backer.back_project(self)
+    backer.back_project(self) if !backer.backed_projects.include?(self)
   end
 end
